8ad453f1b28fa663f6f8e5d3a9e3f18ddae33ab8
Kevin Mahoney <51928306+kevinmahoney135@users.noreply.github.com>
2019-09-07 08:46:14 -0500

    Added initial html, javascript and css code.

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBsEHXCyoCXaQNR1tj2zR6pcl-5FmoNka0",
+    authDomain: "kmahoney135.firebaseapp.com",
+    databaseURL: "https://kmahoney135.firebaseio.com",
+    projectId: "kmahoney135",
+    storageBucket: "",
+    messagingSenderId: "940583215176",
+    appId: "1:940583215176:web:1a83383f00126a5349f87d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
